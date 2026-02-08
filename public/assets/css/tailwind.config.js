759c06ba11903997a8a0e42d65d9758f9b76359b
Gabriel Guerrero <47202574+gguerreroi@users.noreply.github.com>
2026-02-07 20:42:00 -0600

    first commit

---
+++ public/assets/css/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./src/routes/app/*.js",
+      "./src/views/**/*.ejs"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
