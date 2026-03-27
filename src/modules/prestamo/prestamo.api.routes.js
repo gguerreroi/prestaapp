2ca42a14acfe6f60568084c0752e6983d0b70ee6
Gabriel Guerrero <47202574+gguerreroi@users.noreply.github.com>
2026-03-27 16:48:18 -0600

    fix pago

--- src/modules/prestamo/prestamo.api.routes.js
+++ src/modules/prestamo/prestamo.api.routes.js
@@ -1,12 +1,17 @@
-import { Router } from "express"
-import apiAuth from "../../middlewares/auth/api-auth.middleware"
-import requirePermission from "../../middlewares/auth/permission.middleware"
+import { Router } from "express";
+import apiAuth from "../../middlewares/auth/api-auth.middleware";
+import requirePermission from "../../middlewares/auth/permission.middleware";
 import { createLoan } from "./nuevo.prestamo.api.controller";
 import { registrarPago } from "./pagos.prestamo.api.controller";
 
 const router = Router();
 // Base URL: /api/prestamos
-router.post('/nuevo', apiAuth, requirePermission('/prestamos', { isApi: true }), createLoan)
-router.post('/:id', apiAuth, requirePermission('/prestamos', { isApi: true }), registrarPago)
+router.post(
+  "/nuevo",
+  apiAuth,
+  requirePermission("/prestamos", { isApi: true }),
+  createLoan,
+);
+router.post("/:id", apiAuth, registrarPago);
 
 export default router;
