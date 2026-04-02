efdb31b0cfccf266711c3ea4af912969bd9609d9
Gabriel Guerrero <47202574+gguerreroi@users.noreply.github.com>
2026-04-02 13:49:54 -0600

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,49 +1,50 @@
-const mssql = require('mssql')
-const env = require('./env');
+const mssql = require("mssql");
+const env = require("./env");
 
 let pool;
 
 async function connection() {
-	try {
-		// Si ya hay pool conectado, reutilizarlo
-		if (pool?.connected) return pool;
+  try {
+    // Si ya hay pool conectado, reutilizarlo
+    if (pool?.connected) return pool;
 
-		const cfg = {
-			user: env.DB.USER,
-			password: env.DB.PASSWORD,
-			server: env.DB.HOST,
-			database: env.DB.NAME,
-			port: env.DB.PORT,
-			options: {
-				encrypt: env.DB.ENCRYPT,
-				enableArithAbort: true,
-			},
-		};
+    const cfg = {
+      user: env.DB.USER,
+      password: env.DB.PASSWORD,
+      server: env.DB.HOST,
+      database: env.DB.NAME,
+      port: env.DB.PORT,
+      options: {
+        encrypt: env.DB.ENCRYPT,
+        trustServerCertificate: true,
+        enableArithAbort: true,
+      },
+    };
 
-		const connPool = new mssql.ConnectionPool(cfg);
-		pool = await connPool.connect();
+    const connPool = new mssql.ConnectionPool(cfg);
+    pool = await connPool.connect();
 
-		pool.on('error', async (e) => {
-			console.log('on pool error ', e);
-			await close_pool();
-		});
+    pool.on("error", async (e) => {
+      console.log("on pool error ", e);
+      await close_pool();
+    });
 
-		return pool;
-	} catch (e) {
-		pool = null;
-		const message = e?.message || e?.originalError || e;
-		return { code: 502, message: `${message}` };
-	}
+    return pool;
+  } catch (e) {
+    pool = null;
+    const message = e?.message || e?.originalError || e;
+    return { code: 502, message: `${message}` };
+  }
 }
 
 async function close_pool() {
-	try {
-		if (pool) await pool.close();
-	} catch (e) {
-		console.log(e);
-	} finally {
-		pool = null;
-	}
+  try {
+    if (pool) await pool.close();
+  } catch (e) {
+    console.log(e);
+  } finally {
+    pool = null;
+  }
 }
 
 module.exports = { connection, mssql };
